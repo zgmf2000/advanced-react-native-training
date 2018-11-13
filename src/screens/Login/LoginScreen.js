d55f4d4b8f585dfaae52d8b0712873188646b0ae
Bintoro Adi Guna <bintoro.guna@btpn.com>
2018-11-13 11:08:02 +0700

    Finished developing simple login screen

--- src/screens/Login/LoginScreen.js
+++ src/screens/Login/LoginScreen.js
@@ -1,17 +1,57 @@
 // @flow
 
 import * as React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Alert} from 'react-native';
+import {TextField, Button} from '../../core-ui';
 
 type Props = {};
 
+type State = {|
+  username: string,
+  password: string,
+|};
+
 // eslint-disable-next-line
-function LoginScreen(props: Props) {
-  return (
-    <View style={styles.container}>
-      <Text> create your own Login Screen</Text>
-    </View>
-  );
+class LoginScreen extends React.Component<Props, State> {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+
+  _onSubmit = () => {
+    const {username, password} = this.state;
+    Alert.alert(
+      'Submission Success!',
+      `Your username is ${username}, and your password is ${password}`,
+    );
+  };
+
+  _onChangeText = (stateToChange) => (text) => {
+    this.setState({
+      [stateToChange]: text,
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Login</Text>
+        <TextField
+          placeholder="Username"
+          onChangeText={this._onChangeText('username')}
+        />
+        <TextField
+          placeholder="Password"
+          secureTextEntry={true}
+          onChangeText={this._onChangeText('password')}
+        />
+        <Button text="Submit" onPress={this._onSubmit} />
+      </View>
+    );
+  }
 }
 
 let styles = StyleSheet.create({
@@ -19,6 +59,12 @@ let styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    paddingHorizontal: 12,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 32,
+    marginBottom: 12,
   },
 });
 
